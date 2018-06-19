047ca82dfc2288682faa24bcc789078d50ce3566
David M <David M>
2018-06-19 07:35:03 -0300

    modificacion de funcion cargarfecha() a insertarfecha(mes, año)

--- scripts_dw.sql
+++ scripts_dw.sql
@@ -163,6 +163,8 @@ CREATE TABLE VENTAS (
 	CONSTRAINT PK_FACTURA PRIMARY KEY (Id_Factura)
 );
 
+
+/*
 CREATE OR REPLACE FUNCTION CargaTiempo() RETURNS VOID AS
 $$
 DECLARE
@@ -189,6 +191,35 @@ END;
 $$ LANGUAGE plpgsql;
 
 SELECT CargaTiempo();
+*/
+
+CREATE OR REPLACE FUNCTION InsertarTiempo(mesI integer, añoI integer) RETURNS integer AS
+$$
+DECLARE
+	id integer;
+BEGIN
+
+	SELECT id_tiempo FROM public.tiempo WHERE mes=mesI and "año"= añoI into id;
+	IF id IS NULL THEN
+		CASE 
+			WHEN (mesI >= 1 AND mesI <= 3) THEN
+				INSERT INTO TIEMPO (mes,año,trimetres) VALUES (mesI,añoI,1);
+			WHEN (mesI >= 4 AND mesI <= 6) THEN
+				INSERT INTO TIEMPO (mes,año,trimetres) VALUES (mesI,añoI,2);
+				
+			WHEN (mesI >= 7 AND mesI <= 9) THEN
+				INSERT INTO TIEMPO (mes,año,trimetres) VALUES (mesI,añoI,3);
+			ELSE
+				INSERT INTO TIEMPO (mes,año,trimetres) VALUES (mesI,añoI,4);
+		END CASE;
+		SELECT id_tiempo FROM public.tiempo WHERE mes=mesI and "año"= añoI into id;
+	END IF;
+	RETURN id;
+END;
+$$ LANGUAGE plpgsql;
+
+select InsertarTiempo(2,2030);
+
 
 CREATE TABLE tmpVentas (
 	fecha_vta timestamp,
