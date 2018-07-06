60049de9bf31a33ab0fe463696a66c16470fe632
Lucas <88lucas88@gmail.com>
2018-07-06 10:41:30 -0300

    modificaciones en punto consultas

--- consultasAnidamiento.sql
+++ consultasAnidamiento.sql
@@ -2,7 +2,7 @@
 ------------------------------------------------ Punto 4 -----------------------------------------------
 
 -- Venta vista por mes o por año, por sucursal, por región, por cliente y demás combinaciones entre las perspectivas.
-SELECT T.mes, T.año, S.descripcion as sucursal, C.descripcion as ciudad, CL.nombre nombre_cliente, 
+SELECT T.mes, T.año, S.descripcion as sucursal, C.descripcion as Ciudad, CL.nombre nombre_cliente, 
 sum(V.monto_vendido) as monto_total_vendido, sum(V.cantidad_vendida) as cant_total_vendida
 FROM ventas V, tiempo T, sucursal S, ciudad C, clientes CL
 WHERE V.id_tiempo = T.id_tiempo and V.id_sucursal = S.id_sucursal
@@ -14,7 +14,7 @@ GROUP BY CUBE(T.mes, T.año,
 )
 	
 -- Es necesario conocer también de que manera influye, en las ventas de productos, la zona geográfica en la que están ubicados los locales.
-SELECT R.descripcion, P.descripcion, C.descripcion, sum(V.monto_vendido) as monto_total_vendido, sum(V.cantidad_vendida) as cant_total_ventida
+SELECT R.descripcion as Region, P.descripcion as Provincia, C.descripcion as Ciudad, sum(V.monto_vendido) as monto_total_vendido, sum(V.cantidad_vendida) as cant_total_ventida
 FROM ventas V, sucursal S, ciudad C, provincia P, region R
 WHERE V.id_sucursal = S.id_sucursal and S.id_ciudad = C.id_ciudad
 	and C.id_provincia = P.id_provincia and P.id_region = R.id_region
@@ -25,7 +25,7 @@ GROUP BY ROLLUP (
 )
 
 -- De cada cliente se desea conocer cuales son los que generan mayores ingresos a la cooperativa.
-SELECT C.nombre, sum(cantidad_vendida), sum(V.monto_vendido) as total_ingreso, rank() over (order by (sum(V.monto_vendido)) desc) as pos
+SELECT C.nombre as Cliente, sum(cantidad_vendida), sum(V.monto_vendido) as total_ingreso, rank() over (order by (sum(V.monto_vendido)) desc) as pos
 FROM Clientes C, Ventas V
 WHERE C.id_cliente = V.id_cliente
 GROUP BY C.id_cliente
